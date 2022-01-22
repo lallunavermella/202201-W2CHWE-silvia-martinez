1129b92c8d26e25770d7a65c7ed1d45206835991
Silvia Martinez <sil@silvia.com>
2022-01-22 12:06:34 +0100

    update some files in the project

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const cellsAB = [
-  ["A", "A", "A", "B"],
-  ["A", "A", "B", "B"],
-  ["A", "B", "A", "A"],
-  ["A", "A", "A", "A"],
-];
 const row = 2;
 const column = 2;
 const newArrayGame = (rows, columns) => {
@@ -17,3 +11,4 @@ const newArrayGame = (rows, columns) => {
   return boardGame;
 };
 const boardGame = newArrayGame(row, column);
+console.log(boardGame);
